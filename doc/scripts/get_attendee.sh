148bf9f76e7c8cb2601c41452d535ffbcd308bc1
Mario Bodemann <mario.bodemann@gmail.com>
2026-05-13 15:52:38 +0200

    add documentation script

--- doc/scripts/get_attendee.sh
+++ doc/scripts/get_attendee.sh
@@ -1,7 +1,8 @@
 source global_vars.sh
 
 curl --verbose \
-  'https://gdg.community.dev/api/event/'$BEVY_EVENT_ID'/attendee/' \
+  'https://gdg.community.dev/api/user/2914846/' \
   -H "authorization: Token $BEVY_TOKEN" \
   -H 'content-type: application/json' |
+  jq |
   less
